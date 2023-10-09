4ea7dc439a1a483594d975eb864e83fcd7eb5ca7
Katarzyna Popieniuk <katarzynapopieniuk@gmail.com>
2023-10-09 17:58:20 +0200

    fix score counting

--- ZJAZD1/TicketToRide.py
+++ ZJAZD1/TicketToRide.py
@@ -14,7 +14,6 @@ class TicTacToe(TwoPlayerGame):
     def __init__(self, players):
         self.rows = 5
         self.columns = 7
-        self.last_move_position = 0
         self.players = players
         self.board = [0 for i in range(self.rows * self.columns)]
         self.current_player = 1  # player 1 starts.
@@ -24,13 +23,19 @@ class TicTacToe(TwoPlayerGame):
 
     def make_move(self, move):
         self.board[int(move) - 1] = self.current_player
-        self.last_move_position = int(move) - 1
 
     def unmake_move(self, move):  # optional method (speeds up the AI)
         self.board[int(move) - 1] = 0
 
     def lose(self):
-        return False
+        return self.get_score_for_symbol(self.opponent_index) >= 5
+
+    def get_score_for_symbol(self, symbol):
+        score = 0
+        for i in range(1, self.rows * self.columns) :
+            if(self.is_in_center_of_score(i, symbol)) :
+                score = score + 1
+        return score
 
     def is_over(self):
         return (self.possible_moves() == []) or self.lose()
@@ -46,6 +51,8 @@ class TicTacToe(TwoPlayerGame):
             )
         )
 
+        print ("\n" + str(self.get_score_for_symbol(1)) + ":" + str(self.get_score_for_symbol(2)))
+
     def is_position_valid(self, i):
         if i >= (self.rows * self.columns):
             return False
@@ -54,56 +61,34 @@ class TicTacToe(TwoPlayerGame):
         return True
 
     def are_positions_in_row(self, pos1, pos2, pos3):
-        row_number = pos1 % self.columns
-        return row_number == pos2 % self.columns and row_number == pos3 % self.columns
+        first_column_number = (pos1 - 1) // self.columns
+        return first_column_number == (pos2 - 1) // self.columns and first_column_number == (pos3 - 1) // self.columns
 
     def make_three(self, pos1, pos2, pos3):
-
-        if self.is_position_valid(pos1-1) and self.is_position_valid(pos2-1) and self.is_position_valid(pos3-1):
-            symbol = self.board[pos1-1]
-            return symbol == self.board[pos2-1] and symbol == self.board[pos3-1]
+        if self.is_position_valid(pos1 - 1) and self.is_position_valid(pos2 - 1) and self.is_position_valid(pos3 - 1):
+            symbol = self.board[pos1 - 1]
+            temp = symbol == self.board[pos2 - 1] and symbol == self.board[pos3 - 1]
+            return temp
         else:
             return False
 
-    def last_score(self):
-        if self.make_three(self.last_move_position, self.last_move_position - self.columns,
-                           self.last_move_position - 2 * self.columns) \
-                or self.make_three(self.last_move_position, (self.last_move_position + self.columns),
-                                   (self.last_move_position + (2 * self.columns))) \
-                or self.make_three(self.last_move_position, (self.last_move_position - self.columns),
-                                   (self.last_move_position + self.columns)) \
-                or (self.make_three(self.last_move_position, (self.last_move_position - 1),
-                                    (self.last_move_position - 2)) and self.are_positions_in_row(
-                                                                                                 self.last_move_position,
-                                                                                                 self.last_move_position - 1,
-                                                                                                 self.last_move_position - 2)) \
-                or (self.make_three(self.last_move_position, (self.last_move_position + 1),
-                                    (self.last_move_position + 2)) and self.are_positions_in_row(
-                                                                                                 self.last_move_position,
-                                                                                                 self.last_move_position + 1,
-                                                                                                 self.last_move_position + 2)) \
-                or (self.make_three(self.last_move_position, (self.last_move_position - 1),
-                                    (self.last_move_position + 1)) and self.are_positions_in_row(
-                                                                                                 self.last_move_position,
-                                                                                                 self.last_move_position - 1,
-                                                                                                 self.last_move_position + 1)) \
-                or self.make_three(self.last_move_position, (self.last_move_position - self.columns - 1),
-                                   (self.last_move_position - (2 * self.columns + 1))) \
-                or self.make_three(self.last_move_position, (self.last_move_position + self.columns + 1),
-                                   (self.last_move_position + (2 * self.columns + 1))) \
-                or self.make_three(self.last_move_position, (self.last_move_position - self.columns - 1),
-                                   (self.last_move_position + self.columns + 1)) \
-                or self.make_three(self.last_move_position, (self.last_move_position - self.columns + 1),
-                                   (self.last_move_position - (2 * self.columns - 1))) \
-                or self.make_three(self.last_move_position, (self.last_move_position + self.columns - 1),
-                                   (self.last_move_position + (2 * self.columns - 1))) \
-                or self.make_three(self.last_move_position, (self.last_move_position - self.columns + 1),
-                                   (self.last_move_position + self.columns - 1)):
-            return -5
-        return 0
+    def isNotSideBorder(self, pos):
+        return (pos - 1) % self.columns != 0 and (pos - 1) % self.columns != self.columns - 1
+
+    def is_in_center_of_score(self, pos, symbol):
+        if(self.board[pos - 1] != symbol):
+            return False
+
+        if self.make_three(pos, (pos - self.columns), (pos + self.columns)) \
+                or (self.make_three(pos, (pos - 1), (pos + 1)) and self.are_positions_in_row(pos, pos - 1,pos + 1)) \
+                or (self.isNotSideBorder(pos) and self.make_three(pos, (pos - self.columns - 1), (pos + self.columns + 1))) \
+                or  (self.isNotSideBorder(pos) and self.make_three(pos, (pos - self.columns + 1), (pos + self.columns - 1))):
+            return True
+
+        return False
 
     def scoring(self):
-        return -100 if self.lose() else self.last_score()
+        return -100 if self.lose() else 0
 
 
 if __name__ == "__main__":
